e2c66831c46c97b80a83bfdcf75b1ed59ee4149f
Dan Ventura <danice.ventura@gmail.com>
2022-05-20 22:17:13 -0600

    added mobile sizing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <div className="navBar">
-      <h1 className="navName">Dan Ventura</h1>
-      <h3 className="navTitle">Full Stack Software Engineer</h3>
+      <h4 className="navName">Dan Ventura</h4>
+      <h5 className="navTitle">Full Stack Software Engineer | RN</h5>
       <div className="navLinksGrid">
         {/* <Link to="/" style={{ textDecoration: 'none' }} className="routerLink"> */}
-        <h4 className="navLink">Home</h4>
+        <h6 className="navLink">Home</h6>
         {/* </Link> */}
 
-        <h4 className="navLink">About</h4>
-        <h4 className="navLink">Projects</h4>
-        <h4 className="navLink">Contact</h4>
+        <h6 className="navLink">About</h6>
+        <h6 className="navLink">Projects</h6>
+        <h6 className="navLink">Contact</h6>
       </div>
     </div>
   )
